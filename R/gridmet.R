3d035be7a5cf88bbc24a529a8b73bf6d3644f570
Ben Sabath <mbsabath@hsph.harvard.edu>
2020-09-22 11:36:37 -0400

    initial commit

---
+++ R/gridmet.R
@@ -0,0 +1,116 @@
+### Functions to handle downloading of raster cliamte data and aggregate it to zip code
+
+
+#' Download Gridmet data set, aggregate to zip code or county
+#'
+#' @param variable name of the variable to download
+#' @param years vector of years of data to process
+#' @param geography what geography to aggregate to, should be zip code or county
+#' @param geo_type for zip code, should points, polygons, or combined be used.
+#'     must be any value in c('polygon','point','combined') Defaults to combined.
+#' @param temp_dirname where should downloaded data be stored
+#' @param cache should the downloade data be stored
+#'
+#' @importFrom ncdf4 nc_open nc_close
+#' @importFrom raster brick
+#' @export
+get_gridmet <- function(variable,
+                        years = 1999:2019,
+                        geography,
+                        geo_type = NULL,
+                        temp_dirname = "temp",
+                        cache = T) {
+
+  ## Create temporary directory if it doesn't already exist
+  if (!dir.exists(temp_dirname)) {
+    dir.create(temp_dirname)
+  }
+
+  ## download grid met files
+  ## files are small enough (~3GB per variable) that we don't need to worry about storage
+  for (year in years) {
+    download_gridmet(variable, year, temp_dirname)
+  }
+
+  for (year in years) {
+    ## read in ncdf data
+    ncin <- nc_open(paste0(temp_dirname,"/", variable, "_", year, ".nc"))
+    varname <- names(ncin$var)[1]
+    nc_close(ncin)
+
+    rast <- brick(paste0(temp_dirname,"/", variable, "_", year, ".nc"), varname = varname)
+
+  }
+
+
+  ## If we're not caching delete files
+  if (!cache) {
+    lapply(list.files(path = temp_dirname,
+                      pattern = paste0(variable, "_[0-9]{4}.nc"),
+                      full.names = T),
+           file.remove)
+    #if temp dir is empty, delete it too
+    if (length(dir) == 0) {
+      unlink(dir)
+    }
+  }
+
+}
+
+#' Download grid met data and store in the temporary directory
+#'
+#' @param variable Gridmet variable to download
+#' @param year year of data to download
+#' @param temp_dirname directory to store the downloaded file in
+#'
+#' @export
+download_gridmet <- function(variable, year, temp_dirname) {
+
+  url <- paste0("https://www.northwestknowledge.net/metdata/data/", variable, "_", year, ".nc")
+  outname <- paste0(temp_dirname,"/", variable, "_", year, ".nc")
+  utils::download.file(url, outname)
+}
+
+#' Create a data frame of daily zip code area weighted estiamtes
+#'
+#' @importFrom raster nlayers disaggregate
+#' @importFrom velox velox
+#' @import data.table
+process_year_polygon <- function(rast, poly, variable) {
+  out <- NULL
+  for (i in 1:nlayers(rast)) {
+    temp_layer <- rast[[i]]
+    date <- as.Date(temp_layer@z[[1]], origin = "1900-01-01")
+    message(paste("processing",date))
+    temp_layer <- disaggregate(temp_layer, fact = 5)
+    temp_df <- data.table(velox(temp_layer)$extract(poly, small = T, fun = function(x){mean(x, na.rm = T)})[,1])
+    names(temp_df) <- variable
+    temp_df[, date := date]
+    temp_df[, zip := poly$ZIP]
+    out <- rbind(out, temp_df)
+  }
+
+  return(out)
+}
+
+process_year_point <- function() {
+  out <- NULL
+  for (i in 1:nlayers(rast)) {
+    temp_layer <- rast[[i]]
+    date <- as.Date(temp_layer@z[[1]], origin = "1900-01-01")
+    message(paste("processing",date))
+    temp_df <- data.table(velox(temp_layer)$extract_point(poly, small = T, fun = function(x){mean(x, na.rm = T)})[,1])
+    names(temp_df) <- variable
+    temp_df[, date := date]
+    temp_df[, zip := poly$ZIP]
+    out <- rbind(out, temp_df)
+  }
+
+  return(out)
+
+}
+
+
+
+
+
