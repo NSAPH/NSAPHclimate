fa6c9ccbef83579b659550c1a81a5970dc2f1d4d
Ben Sabath <mbsabath@hsph.harvard.edu>
2020-09-25 13:02:46 -0400

    zip code shape function

---
+++ R/geographies.R
@@ -0,0 +1,36 @@
+## Geographies.R
+## Code to handle reading/processing shape files
+
+#' Read in polygon and point shape from the standard
+#' zip code structure
+#' @param shape_path path to the directory containing the zip code shape files
+#' @param year year of zip codes to grab
+#'
+#' @return a list with the polygon and point shpae files foor a given year
+#' @details Assumes the folder structure in the zip_for_loop directory
+#' @importFrom sf st_as_sf read_sf
+#' @importFrom stringr str_c str_sub
+#' @export
+get_year_zip_shapefiles <- function(shape_path, year) {
+  if (year < 2000) year <- 2000 # no files pre 2000 available, use 2000
+  if (year >= 2018) year <- 2017 #no file after 2017, use 2017
+
+  polyfiles <- list.files(str_c(shape_path, "polygon", sep = "/"),
+                          full.names = TRUE,
+                          pattern = "\\.shp$")
+  pointfiles <- list.files(str_c(shape_path, "pobox_csv", sep = "/"),
+                           full.names = TRUE,
+                           pattern = "\\.csv$")
+  year_abbrev <- str_sub(year, 3, 4)
+  polyfile <- str_subset(polyfiles, str_c(year_abbrev, "USZIP", sep = ""))
+  pointfile <- str_subset(pointfiles, str_c(year_abbrev, "USZIP", sep = ""))
+
+  out <- list()
+  out$point <- fread(pointfile)
+  out$point <- st_as_sf(out$point, coords = c("POINT_X", "POINT_Y"))
+  out$poly <- read_sf(polyfile)
+
+
+  return(out)
+
+}
